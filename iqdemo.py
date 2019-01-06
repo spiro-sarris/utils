2e4d7f6a951c15b5d0d079a85ec27bbb1a0abbd9
spiro-sarris <spiro.sarris@gmail.com>
2019-01-06 23:19:09 +0700

    finished max hold function

--- iqdemo.py
+++ iqdemo.py
@@ -11,11 +11,11 @@ t_end = 4000e-6					# length of time
 nsamples = t_end*fs				# number of samples total
 tt = np.arange(0,nsamples)/fs	# time values in seconds
 phi0 = 0						# phase initial
-freq = 50e3						# frequency
+freq = 50e3					# frequency
 
 # Settings for frequency domain spectrum
-fftsize = 1024					# number of FFT spectrum bins
-ff_kHz = (np.linspace(0,fs,fftsize)-fs/2)/1e3
+fftsize = 1000					# number of FFT spectrum bins
+ff_kHz = (np.linspace(0,fs,fftsize))/1e3
 
 # Generate a complex-valued signal
 sig_c = np.exp(1j*(2*np.pi*freq*tt+phi0))
@@ -27,9 +27,9 @@ sig_r = np.cos(2*np.pi*freq*tt+phi0)
 sig_h = scipy.signal.hilbert(sig_r)
 
 # FFT spectrum of each signal
-SIG_C = np.fft.fft(sig_c, fftsize)
-SIG_R = np.fft.fft(sig_r, fftsize)
-SIG_H = np.fft.fft(sig_h, fftsize)
+SIG_C = mylib.fft_maxhold(sig_c, fftsize, window=False)
+SIG_R = mylib.fft_maxhold(sig_r, fftsize, window=False)
+SIG_H = mylib.fft_maxhold(sig_h, fftsize, window=False)
 
 # FFTSHIFT wrap negative frequencies around to the left
 SIG_C_SHIFT = np.fft.fftshift(SIG_C)
@@ -85,7 +85,7 @@ plt.legend(loc=3)
 fig = plt.figure(4, figsize=figure_size)
 ax1 = plt.subplot(111)
 plt.title('FFT Spectrum of Complex-Valued Signal')
-plt.plot(ff_kHz, mylib.todB(np.abs(SIG_C_SHIFT)**2))
+plt.plot(ff_kHz, SIG_C)
 plt.xlabel('frequency(kHz)')
 plt.xlim((ff_kHz[0],ff_kHz[-1]))
 plt.grid(True)
@@ -93,7 +93,7 @@ plt.grid(True)
 fig = plt.figure(5, figsize=figure_size)
 ax1 = plt.subplot(111)
 plt.title('FFT Spectrum of Real-Valued Signal')
-plt.plot(ff_kHz, mylib.todB(np.abs(SIG_R_SHIFT)**2))
+plt.plot(ff_kHz, SIG_R)
 plt.xlabel('frequency(kHz)')
 plt.xlim((ff_kHz[0],ff_kHz[-1]))
 plt.grid(True)
@@ -101,7 +101,7 @@ plt.grid(True)
 fig = plt.figure(6, figsize=figure_size)
 ax1 = plt.subplot(111)
 plt.title('FFT Spectrum of Hilbert Transformed Real Signal')
-plt.plot(ff_kHz, mylib.todB(np.abs(SIG_H_SHIFT)**2))
+plt.plot(ff_kHz, SIG_H)
 plt.xlabel('frequency(kHz)')
 plt.xlim((ff_kHz[0],ff_kHz[-1]))
 plt.grid(True)
--- mylib.py
+++ mylib.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.signal
 
 def todB(linearVal):
 	return 10*np.log10(linearVal)
@@ -6,3 +7,31 @@ def todB(linearVal):
 def toLinear(dBVal):
 	return 10**(dBVal/10)
 	
+def fft_maxhold(sigt_1D, fft_size=1024, window=False):
+	# Caluclate number of columns to split the long 1-D array.
+	nrows = int(np.ceil(float(sigt_1D.size) / fft_size))
+	
+	# Zero pad the end of the array to prepare for reshape dimensions
+	sigt_1D_padded = np.pad(sigt_1D,(0,int(nrows*fft_size-sigt_1D.size)),'constant')
+	# Reshape the 1-D array into 2-D array with each column the 
+	# same length as FFT sizes
+	sigt_2D = np.reshape(sigt_1D_padded, (nrows, fft_size))
+	
+	# Generate a window function for apodization
+	if(window==True):
+		w = scipy.signal.windows.blackmanharris(fft_size)
+	else:
+		w = np.ones(fft_size)
+
+	# Tile the window across all rows and apply window
+	w_2D = np.tile(w,(nrows,1))
+	sigt_2D_w = w_2D*sigt_2D
+
+	# Calculate FFT of each column
+	sigf_2D = np.fft.fft(sigt_2D_w, fft_size, axis=1)
+	
+	# Calculate magnitude in dB
+	sigf_maxhold = np.sum(np.abs(sigf_2D),axis=0)/nrows/fft_size
+	sigf_maxhold_mag = 10*np.log10(sigf_maxhold)
+	
+	return sigf_maxhold_mag
