cdc9b01b435a449f1d07c117f7e07ec408d59e53
Spiro Sarris <spiro.sarris@gmail.com>
2019-08-18 21:16:07 +0700

    add PFD

--- survey_gistda.py
+++ survey_gistda.py
@@ -56,20 +56,58 @@ for fileinfo in list_of_files_ant:
 	# Iterate through files and process each one
 	# Read data from files
 	data = fieldfox.read_sa_csv(fileinfo[0])
+	
+	# Scale measured power [dBm] to power flux in units of dB(W/m^2)
+	# Assume antenna gain is constant across all frequency.  In general,
+	# The gain vs. frequency data should be used, but for this Aaronia 60180,
+	# I will approximate 5 dBi across the range of frequency
+	speed_of_light_m_per_sec = 299792458
+	W_per_mW = 0.001
+	wavelength_m = speed_of_light_m_per_sec/data['ff_Hz']
+	rx_antenna_gain_dBi = 5*np.ones(data['ff_Hz'].shape[0])
+	rx_antenna_gain_linear = 10**(rx_antenna_gain_dBi/10)
+	rx_antenna_effective_area_m2 = rx_antenna_gain_linear*(wavelength_m**2)/(4*np.pi) 
+
+	power_maxhold_W = W_per_mW*(10**(data['maxhold_dBm']/10))
+	power_avg_W = W_per_mW*(10**(data['avg_dBm']/10))
+	
+	power_flux_maxhold_W_per_m2 = power_maxhold_W/rx_antenna_effective_area_m2
+	power_flux_avg_W_per_m2 = power_avg_W/rx_antenna_effective_area_m2
+
+	# Power Flux Spectral Density (PFD)
+	# Normalize flux to a reference bandwidth dB(W/m^2/REF_Hz)
+	# Spectrum analyzer resolution bandwidth was set to fixed 5 MHz for the entire
+	# measurement session.  In general, this value should be read from line 15 of the
+	# .csv file at parameter "ResolutionBW"
+	specA_resolutionBW = 5e6
+	reference_bw_Hz = 5e6
+	bw_scale = float(reference_bw_Hz)/float(specA_resolutionBW)
+
+	pfd_maxhold_W_per_m2_per_BW = bw_scale*power_flux_maxhold_W_per_m2
+	pfd_avg_W_per_m2_per_BW = bw_scale*power_flux_avg_W_per_m2
+
+	pfd_maxhold_dBW_per_m2_per_BW = 10*np.log10(pfd_maxhold_W_per_m2_per_BW)
+	pfd_avg_dBW_per_m2_per_BW = 10*np.log10(pfd_avg_W_per_m2_per_BW)
+	
+	# Prepare file name
 	filename_w_ext = os.path.basename(fileinfo[0])
 	filename, file_extension = os.path.splitext(filename_w_ext)
 	# Make plots
 	str_title = "File %s, AZ=%d [deg]" % (filename_w_ext, fileinfo[1])
 	figure_size = (10,8)
 	xlabel = "Frequency [GHz]"
-	ylabel = "Power [dBm] Received at SA using 5 dBi Test Antenna"
-	ymin = -100
+	ylabel = "Power Flux [dBW/m^2] in %7.3f MHz BW" % (reference_bw_Hz/1e6)
+	ymin = -120
 	ymax = -10
 
 	fig1 = plt.figure(os.path.basename(fileinfo[0]), figsize=figure_size)
 	ax1 = plt.subplot(1,1,1)
-	ax1.plot(data['ff_Hz']*1e-9, data['maxhold_dBm'],'b', label='maxhold')
-	ax1.plot(data['ff_Hz']*1e-9, data['avg_dBm'],'r', label='average 1000')
+	# ax1.plot(data['ff_Hz']*1e-9, 10*np.log10(rx_antenna_effective_area_m2),'k', label='effective area')
+	# ax1.plot(data['ff_Hz']*1e-9, data['maxhold_dBm'],'g', label='maxhold specA meas')
+	# ax1.plot(data['ff_Hz']*1e-9, data['avg_dBm'],'m', label='average 1000 specA meas')
+	ax1.plot(data['ff_Hz']*1e-9, pfd_maxhold_dBW_per_m2_per_BW,'b', label='maxhold')
+	ax1.plot(data['ff_Hz']*1e-9, pfd_avg_dBW_per_m2_per_BW,'r', label='average 1000')
+	
 	ax1.set_ylim((ymin,ymax))
 	ax1.set_title(str_title)
 	ax1.set_xlabel(xlabel)
